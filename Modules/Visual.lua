01dba2c854bf688b78031ec879508949fc38ec2a
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-01 00:22:47 +0300

    Update Visual.lua

--- Modules/Visual.lua
+++ Modules/Visual.lua
@@ -27,4 +27,36 @@ function Visual:hit_particle()
 end
 
 
+function Visual:change_ball()
+    local asset = self.assets.Balls:FindFirstChild(self.asset_name)
+
+    if not asset then
+        warn(`asset {self.asset_name} not found. Try using these assets: {unpack(self.assets.Balls:GetChildren())}`)
+
+        return
+    end
+
+    asset = asset:Clone()
+    asset.Parent = workspace.Balls
+    asset.Weld.Part1 = self.ball
+end
+
+
+function Visual:remove_ball()
+    for _, object in self.ball:GetChildren() do
+        if not object:IsA('Part') then
+            continue
+        end
+    
+        local ball_asset = self.assets.Balls:FindFirstChild(object.Name)
+
+        if not ball_asset then
+            continue
+        end
+
+        ball_asset:Destroy()
+    end
+end
+
+
 return Visual
