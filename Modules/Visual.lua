e613e4986891f0fe0544f52f31a4c10bcf8936ab
aviorety <101004266+aviorety@users.noreply.github.com>
2024-04-30 18:19:58 +0300

    Create Visual.lua

---
+++ Modules/Visual.lua
@@ -0,0 +1,25 @@
+local Visual = {}
+
+
+function Visual:hit_particle()
+    local asset = self.assets.HitParticles:FindFirstChild(self.asset_name)
+    
+    if not object then
+        return
+    end
+
+    asset = object:Clone()
+    asset.Parent = workspace.Terrain
+    asset.Position = self.origin
+
+    for _, object in asset:GetChildren() do
+        if not object:IsA('ParticleEmmiter') then
+            continue
+        end
+
+        object:Emit(object:GetAttribute('EmitCount'))
+    end
+end
+
+
+return Visual
