d865653820cdf46ac3237dc02ec89fb273618c20
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-05 16:51:10 +0300

    t

---
+++ Modules/Animation.lua
@@ -0,0 +1,48 @@
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local Animation = {}
+Animation.storage = {
+    [`Astral Sword`] = ReplicatedStorage.Misc.SwordPacksVFX[`Astral Sword`],
+    [`Crimson Katana`] = ReplicatedStorage.Misc.SwordPacksVFX[`Crimson Katana`],
+    [`Crystal Hammer`] = ReplicatedStorage.Misc.SwordPacksVFX[`Crystal Hammer`],
+    [`Crystal Scissors`] = ReplicatedStorage.Misc.SwordPacksVFX[`Crystal Scissors`],
+    [`Cupid's Bow`] = ReplicatedStorage.Misc.SwordPacksVFX[`Cupid's Bow`],
+    [`Dual Crystal Hammer`] = ReplicatedStorage.Misc.SwordPacksVFX[`Dual Crystal Hammer`],
+    [`Dual Shadow Mirage`] = ReplicatedStorage.Misc.SwordPacksVFX[`Dual Shadow Mirage`],
+    [`Dual Stardust Katana`] = ReplicatedStorage.Misc.SwordPacksVFX[`Dual Stardust Katana`],
+    [`Dual Void Scythes`] = ReplicatedStorage.Misc.SwordPacksVFX[`Dual Void Scythes`],
+    [`Heavenly Sword`] = ReplicatedStorage.Misc.SwordPacksVFX[`Heavenly Sword`],
+    [`Lunar Parasol`] = ReplicatedStorage.Misc.SwordPacksVFX[`Lunar Parasol`],
+    [`Shadow Mirage`] = ReplicatedStorage.Misc.SwordPacksVFX[`Shadow Mirage`],
+    [`Void Scythe`] = ReplicatedStorage.Misc.SwordPacksVFX[`Void Scythe`]
+}
+Animation.playing = false
+
+
+function Animation:play()
+    if not Animation.storage[self.asset] then
+        return
+    end
+
+    local animation_asset = Animation.storage[self.asset]:FindFirstChild('Animation')
+
+    if not animation_asset then
+        warn(`Animation asset hasn't been found in {self.asset}`)
+    end
+
+    Animation.playing = true
+
+    local animation = self.humanoid:LoadAnimation(animation_asset)
+    animation:Play()
+
+    repeat
+        task.wait()
+    until not Animation.playing or not animation
+
+    if animation then
+        animation:Play()
+    end
+end
+
+
+return Animation
