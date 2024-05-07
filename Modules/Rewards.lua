09da5db6d59d91b70ad1ae14b1ba034a2c86fcfa
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-07 17:28:13 +0300

    t

--- Modules/Rewards.lua
+++ Modules/Rewards.lua
@@ -9,20 +9,23 @@ function Rewards:redeem_quests_type()
 end
 
 
-function Rewards:claim_playtime_reward()
+function Rewards:claim_playtime_rewards()
     for index = 1, 6 do
         ReplicatedStorage.Packages._Index['sleitnick_net@0.1.0'].net['RF/ClaimPlaytimeReward']:InvokeServer(index)
     end
 end
 
 
-function Rewards:claim_all_daily_missions()
+function Rewards:claim_clan_quests()
     ReplicatedStorage.Packages._Index['sleitnick_net@0.1.0'].net['RF/ClaimAllDailyMissions']:InvokeServer('Daily')
     ReplicatedStorage.Packages._Index['sleitnick_net@0.1.0'].net['RF/ClaimAllDailyMissions']:InvokeServer('Weekly')
+    ReplicatedStorage.Packages._Index['sleitnick_net@0.1.0'].net['RF/ClaimAllClanBPQuests']:InvokeServer()
 end
 
 
-function Rewards:claim_new_daily_login_reward()
+function Rewards:claim_login_reward()
+    ReplicatedStorage.Remote.RemoteEvent:FireServer('ClaimLoginReward')
+
     for index = 1, 30 do
         ReplicatedStorage.Remote.RemoteFunction:InvokeServer('ClaimNewDailyLoginReward', index)  
     end
@@ -31,12 +34,9 @@ end
 
 function Rewards:claim_rewards()
     Rewards.redeem_quests_type()
-    Rewards.claim_playtime_reward()
-    Rewards.claim_all_daily_missions()
-    Rewards.claim_new_daily_login_reward()
-
-    ReplicatedStorage.Remote.RemoteEvent:FireServer('ClaimLoginReward')
-    ReplicatedStorage.Packages._Index['sleitnick_net@0.1.0'].net['RF/ClaimAllClanBPQuests']:InvokeServer()
+    Rewards.claim_playtime_rewards()
+    Rewards.claim_clan_quests()
+    Rewards.claim_login_reward()
 end
 
 
