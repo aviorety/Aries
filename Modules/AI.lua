a3802a9bd6e71459b40862539f084d5b50d90e37
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-09 04:47:53 +0300

    t

--- Modules/AI.lua
+++ Modules/AI.lua
@@ -17,7 +17,7 @@ AI.last_double_jump = tick()
 
 
 function AI:random_jump()
-    if (tick() - AI.last_jump) < math.random(1, 100) then
+    if (tick() - AI.last_jump) < math.random(1, 50) then
         return
     end
 
@@ -43,14 +43,14 @@ function AI:find_path()
     if (tick() - AI.offset_delay) >= 0.5 then
         local distance = 50 - (self.ball.Velocity.Magnitude / 10)
 
-        local direction = self.target_root.CFrame.LookVector
-        local offset = direction * distance
+        local direction = (self.target_root.Position - LocalPlayer.Character.HumanoidRootPart.Position).Unit
+        local offset = self.target_root.CFrame + (-direction * distance)
 
-        AI.offset = self.target_root.CFrame + offset
+        AI.offset = offset
         AI.offset_delay = tick()
     end
 
-    AI.goal = AI.goal:Lerp(AI.offset, 0.04)
+    AI.goal = AI.goal:Lerp(AI.offset, 0.08)
 
     local passed_distance = (AI.goal.Position - AI.last_goal.Position).Magnitude
 
