77b13021f432c73dbb47be9ed613f8b5cf3c0f11
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-10 23:19:12 +0300

    t

--- Library/Section.lua
+++ Library/Section.lua
@@ -4,16 +4,32 @@ local Section = {}
 function Section:update()
     for _, object in self.container:GetChildren() do
         if not object.Name:find('Section') then
-            return
+            continue
         end
 
         if object == self.left_section then
+            TweenService:Create(object, TweenInfo.new(0.4), {
+                Position = UDim2.new(0.297, 0, 0.041, 0)
+            }):Play()
+
             continue
         end
 
         if object == self.right_section then
+            TweenService:Create(object, TweenInfo.new(0.4), {
+                Position = UDim2.new(0.648, 0, 0.041, 0)
+            }):Play()
+
             continue
         end
+
+        TweenService:Create(object, TweenInfo.new(0.4), {
+            Position = UDim2.new(0.648, 0, 1, 0)
+        }):Play()
+
+        TweenService:Create(object, TweenInfo.new(0.4), {
+            Position = UDim2.new(0.297, 0, 1, 0)
+        }):Play()
 
         object.Visible = false
     end
