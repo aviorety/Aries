ec8d0213b3c823cf7fb9cf64aead1628c6ccc570
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-11 00:00:08 +0300

    t

--- Library/Section.lua
+++ Library/Section.lua
@@ -39,7 +39,7 @@ end
 
 
 function Section:create()
-    local section = game:GetObjects('rbxassetid://17448765120')[1]
+    local section = game:GetObjects('rbxassetid://17449483108')[1]
     section.Parent = self.side
     section.TopBackground.SectionName.Text = self.name
 
