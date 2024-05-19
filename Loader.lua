6d401954ebfe57f427f995662abd34630ef2ce38
aviorety <101004266+aviorety@users.noreply.github.com>
2024-05-20 01:40:19 +0300

    t

---
+++ Loader.lua
@@ -0,0 +1,47 @@
+local Loader = {}
+Loader.http = 'https://raw.githubusercontent.com/aviorety/Aries/main/Games/'
+
+
+function Loader:check()
+    local success, result = pcall(function()
+        return game:HttpGet(Loader.http..self)
+    end)
+
+    if not success then
+        warn(`couldn't access to the {Loader.http}`)
+
+        return
+    end
+
+    return result
+end
+
+
+function Loader:support()
+    local success, executor = pcall(function()
+        return identifyexecutor()
+    end)
+
+    if not success or not executor then
+        warn(`couldn't identify the executor`)
+
+        return
+    end
+    
+    return executor ~= 'Solara'
+end
+
+
+function Loader:__init()
+    if not Loader.support() then
+        return
+    end
+
+    local __loadstring = Loader.check(game.GameId)
+
+    if not __loadstring then
+        warn(`{game.GameId} is not supported`)
+
+        return
+    end
+end
